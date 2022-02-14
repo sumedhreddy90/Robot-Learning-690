6e1adb343be6f9809199b5af0a9b0fed5d2556b4
Sumedh Reddy <sumedhreddykoppula@gmail.com>
2022-02-14 17:19:38 -0500

    Creating CMAC method for training and testing model

--- HW2/code/CMAC.py
+++ HW2/code/CMAC.py
@@ -1,4 +1,5 @@
 from cmath import pi
+from operator import le
 import numpy as np
 from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
@@ -20,7 +21,6 @@ def dataGenerator(input):
     step_size_rad = float(step_size*(np.pi/180))
     x = [step_size_rad * (i + 1) for i in range(0, data_points)]
     y = [input(x[i]) for i in range(0, data_points)]
-    print(x,y)
     #generating training and test data sets (30 test, 70 train)
     x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.3)
     index_train = [x.index(i) for i in x_train]
@@ -28,5 +28,29 @@ def dataGenerator(input):
 
     return [x, y, x_train, x_test, y_train, y_test, index_train, index_test, step_size_rad]
 
+# Defining CMAC whose inputs are either discrete or continous
+def CMAC(input):
 
-dataset = dataGenerator(np.sin)
+    return e_train, e_test
+
+# Initializing neccessary global variables
+# Train and Test data for training the model
+data = dataGenerator(np.sin)
+x_data = data[0]
+y_data = data[1]
+train_x_data = data[2]
+train_y_data = data[4]
+train_index = data[6]
+test_x_data = data[3]
+test_y_data = data[5]
+test_index = data[7]
+step_size = data[8]
+size_x_data = len(x_data)
+size_train_data = len(train_x_data)
+size_test_data = len(test_x_data)
+# Length of input data 100
+print("Length of generated input data: ", size_x_data)
+print("Length of train data:",size_train_data)
+print("Length of test data:",size_test_data)
+CMAC_train_y = [0]
+weights = [0 for i in range(0,size_x_data)]
