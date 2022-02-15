16db0031ea8c9ac66083c5ef13278f09169e4412
Sumedh Reddy <sumedhreddykoppula@gmail.com>
2022-02-14 22:30:43 -0500

    defining local threshold and gneralization for training model

--- HW2/code/CMAC.py
+++ HW2/code/CMAC.py
@@ -31,8 +31,36 @@ def dataGenerator(input):
     return [x, y, x_train, x_test, y_train, y_test, index_train, index_test, step_size_rad]
 
 def CMAC_train():
+    e =1000 
+    for i in range(0, size_train_data):
+       convergence = False
+       t_index = train_index[i]
+       err = 0
+       iter = 0
+       # generalization factor: offset value
+       o_val = 0
+       if i - n_index < 0:
+           o_val = i - n_index
+       if i + n_index >= size_train_data:
+           o_val = size_train_data - (i + n_index)
 
-    return true
+        # iterate through untill convergence is obtained
+       while convergence is False:
+            c_out = 0
+            for j in range(0, GF):
+                total_n_index = t_index - (j - n_index)
+
+                if total_n_index >=0 and total_n_index < x_data:
+                    weights[total_n_index] = weights[total_n_index] + (err / (GF + o_val)) * learn_rate
+                    c_out = c_out + x_data[total_n_index] * weights[total_n_index]
+            err = train_y_data[i] - c_out
+            iter = iter + 1
+            if iter > 35:
+                break
+            # convergence threshold 0.001
+            if abs(math.pow((train_y_data[i] - c_out),2)) <= 0.001:
+               convergence = True 
+
 def CMAC_test():
 
     return true
@@ -75,3 +103,8 @@ print("Length of train data:",size_train_data)
 print("Length of test data:",size_test_data)
 CMAC_train_y = [0]
 weights = [0 for i in range(0,size_x_data)]
+# generalization factor
+GF = 5
+# neighbourhood index
+n_index = int(math.floor(GF / 2))
+learn_rate = 0.15
